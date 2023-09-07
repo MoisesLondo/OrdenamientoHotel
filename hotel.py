bbec79c4ce1aed02437f997c053f15ef6e3b2686
Ricardo Hernandez <ric.and.mail@gmail.com>
2023-09-07 15:14:25 -0400

    Agregados datos faltantes

--- hotel.py
+++ hotel.py
@@ -1,36 +1,59 @@
 import csv
+
 class Reservacion:
-    def __init__(self,nombre, edad,fecha):
+    def __init__(self,id, nombre, habitacion, tipo, precio, num_personas, reserva, entrada, salida, duracion):
+        self.id = id
         self.nombre = nombre
-        self.edad = edad
-        self.fecha = fecha
+        self.habitacion = habitacion
+        self.tipo = tipo
+        self.precio = precio
+        self.num_personas = num_personas
+        self.reserva = reserva
+        self.entrada = entrada
+        self.salida = salida
+        self.duracion = duracion
+        # Agregue los datos que faltaban
 
 def leerArchivo(personas):
     with open("hotel.csv","r") as archivo:
         lector_csv = csv.reader(archivo,delimiter=";")
         for fila in lector_csv:
-            nombre = fila[0]
-            edad = fila[1]
-            fecha = fila[2]
-            reservacion = Reservacion(nombre,edad,fecha)
+            iden, nombre, habitacion, tipo, precio, num_personas, reserva, entrada, salida, duracion = fila
+            #Las variables se pueden asignar solas, si tiene el mismo numero de datos
+            
+            reservacion = Reservacion(iden, nombre, habitacion, tipo, precio, num_personas, reserva, entrada, salida, duracion)
             personas.append(reservacion)
     return personas
+
 def imprimir(personas):
+    # Ahora imprime los nuevos datos (esto hay que cambiarlo despues para que lo imprima como tabla)
     for r in personas:
-        print(r.nombre, r.edad, r.fecha)
+        print("""
+        ID: {}
+        NOMBRE: {}
+        HABITACION: {}
+        TIPO DE HAB: {}
+        PRECIO: {}
+        CANTIDAD DE PERSONAS: {}
+        FECHA DE RESERVACION: {}
+        FECHA DE ENTRADA: {}
+        FECHA DE SALIDA: {}
+        DURACION DE ESTADIA: {}
+        """.format(r.id, r.nombre, r.habitacion, r.tipo, r.precio, r.num_personas, r.reserva, r.entrada, r.salida, r.duracion))
+        
 def main():
     while True:
+        personas = leerArchivo([]) # Lo pongo aqui para que se actualize la lista despues de cada operacion 
 
         print(
-    """
-    Seleccione una opcion:
-    1 - Imprimir datos
-    0 - salir
-    """)
+        """
+        Seleccione una opcion:
+        1 - Imprimir datos
+        0 - salir
+        """)
+        
         opcion = int(input("> "))
 
         if opcion == 1:
-            personas = []
-            leerArchivo(personas)
             imprimir(personas)
 main()
